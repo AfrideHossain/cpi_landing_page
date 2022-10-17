a00723cbef3230080e1468a0265afd71247a1b57
Afride Hossain <54279218+AfrideHossain@users.noreply.github.com>
2022-10-17 21:59:27 +0600

    authentication handler added

--- auth/auth_handler.php
+++ auth/auth_handler.php
@@ -1,9 +1,6 @@
 <?php
 $rawdata = file_get_contents("php://input");
-// echo $rawdata;
 $paramDataObj = json_decode($rawdata);
-// print_r($paramDataObj);
-// echo $paramDataObj->username . " " . $paramDataObj->password . " " . $paramDataObj->user_type;
 
 //import database connection
 include_once "../config/dbcon.php";
@@ -24,14 +21,41 @@ $user_type = string_sanitize($paramDataObj->user_type);
 //login credentials comparison for login
 if ($user_type == "teacher") {
     $sql = "SELECT id, password FROM management WHERE username = '" . mysqli_real_escape_string($conn, $username) . "'";
-    // echo $sql;
+
     $result = mysqli_query($conn, $sql);
-    // print_r($result);
     if (mysqli_num_rows($result) > 0) {
-        while ($row = mysqli_fetch_assoc($result)) {
-            print_r($row);
+        $row = mysqli_fetch_assoc($result); //fetch result as an associative array.
+        if ($row["password"] == $password) {
+            setcookie("uid", $row["id"], time() + 86400, "/");
+            $msg_arr = array("success" => "true", "user_type" => $user_type, "msg" => "login credentials matched");
+            echo json_encode($msg_arr);
+        } else {
+            $msg_arr = array("success" => "false", "msg" => "incorrect password");
+            echo json_encode($msg_arr);
         }
-    }else {
-        echo json_encode({})
+    } else {
+        $msg_arr = array("success" => "false", "msg" => "incorrect username");
+        echo json_encode($msg_arr);
     }
+} elseif ($user_type == "student") {
+    $sql = "SELECT s_id, password FROM students WHERE username = '" . mysqli_real_escape_string($conn, $username) . "'";
+
+    $result = mysqli_query($conn, $sql);
+    if (mysqli_num_rows($result) > 0) {
+        $row = mysqli_fetch_assoc($result); //fetch result as an associative array.
+        if ($row["password"] == $password) {
+            setcookie("sid", $row["s_id"], time() + 86400, "/");
+            $msg_arr = array("success" => "true", "user_type" => $user_type, "msg" => "login credentials matched");
+            echo json_encode($msg_arr);
+        } else {
+            $msg_arr = array("success" => "false", "msg" => "incorrect password");
+            echo json_encode($msg_arr);
+        }
+    } else {
+        $msg_arr = array("success" => "false", "msg" => "incorrect username");
+        echo json_encode($msg_arr);
+    }
+} else {
+    $msg_arr = array("success" => "false", "msg" => "invalid user type");
+    echo json_encode($msg_arr);
 }
